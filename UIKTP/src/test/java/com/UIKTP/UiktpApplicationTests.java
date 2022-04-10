bc1746e6ed1f32e22fd2939c142c336cf4964b03
Rosana-Todosieva <93923104+Rosana-Todosieva@users.noreply.github.com>
2022-04-10 22:36:18 +0200

    Add files via upload

---
+++ UIKTP/src/test/java/com/UIKTP/UiktpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UIKTP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiktpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
