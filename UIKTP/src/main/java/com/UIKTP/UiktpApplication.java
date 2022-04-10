bb7f19b30868c7651166223531ee085dab44cbad
Rosana-Todosieva <93923104+Rosana-Todosieva@users.noreply.github.com>
2022-04-10 22:31:06 +0200

    Add files via upload

---
+++ UIKTP/src/main/java/com/UIKTP/UiktpApplication.java
@@ -0,0 +1,13 @@
+package com.UIKTP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiktpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiktpApplication.class, args);
+	}
+
+}
